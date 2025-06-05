1e2c6a1a7506aadb8e9661412231d06cc5d7c753
한인혁 <138432939+R4mel@users.noreply.github.com>
2025-06-05 14:11:05 +0900

    docerk

--- backend/schema/image_schema.py
+++ backend/schema/image_schema.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 
 class PromptRequest(BaseModel):
@@ -7,3 +8,11 @@ class PromptRequest(BaseModel):
 
 class PromptResponse(BaseModel):
     url: str
+
+
+class ImageDTO(BaseModel):
+    id: str
+    prompt: str
+    save_path: str
+    s3_url: str
+    created_at: datetime
